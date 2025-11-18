a38151bda4d0047920824b19fcf11492049e840f
pedrooayres <pedrohayres@yahoo.com.br>
2025-11-18 13:10:06 +0000

    upadte main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Desenvolvedor dev = new Desenvolvedor("Bernardo Dev");
+        Desenvolvedor dev = new Desenvolvedor("Joao Dev");
         Carreira carreira = new Carreira(dev);
         Carreira.loadingBar(Color.BLUE + "Inicializando Sistema..." + Color.RESET, 20, 80);
         Carreira.loadingBar(Color.CYAN + "Preparando Menu..." + Color.RESET, 15, 60);
